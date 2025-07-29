6de8e41e81c26319c404ad51ce029240953ea8e6
xeiNg0de <alekseytilte@gmail.com>
2025-07-30 01:32:29 +0300

    add parallel

--- img-to-ten/converter.py
+++ img-to-ten/converter.py
@@ -1,4 +1,5 @@
 import os
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 from PIL import Image
 from torchvision import transforms
@@ -18,26 +19,35 @@ def new_dino_size(size: tuple[int, int]) -> tuple[int, int]:
     return 14 * (x // 14), 14 * (y // 14)
 
 
-image_files = [f for f in os.listdir(SRC_DIR) if f.lower().endswith((".jpg", ".jpeg", ".png"))]
-
-for fname in tqdm(image_files[:1000], desc="Transform to tensors"):
+def convert(fname):
+    transform = transforms.Compose([
+            # transforms.Resize(new_dino_size(img.size), interpolation=transforms.InterpolationMode.BICUBIC),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=(0.485, 0.456, 0.406),
+                std=(0.229, 0.224, 0.225),
+            ),
+        ])
+    
     src_path = os.path.join(SRC_DIR, fname)
 
     base_name = os.path.splitext(fname)[0]
     dst_path = os.path.join(DST_DIR, base_name + ".pt")
 
     img = Image.open(src_path).convert("RGB")
-
-    transform = transforms.Compose([
-        # transforms.Resize(new_dino_size(img.size), interpolation=transforms.InterpolationMode.BICUBIC),
-        transforms.CenterCrop(new_dino_size(img.size)),
-        transforms.ToTensor(),
-        transforms.Normalize(
-            mean=(0.485, 0.456, 0.406),
-            std=(0.229, 0.224, 0.225),
-        ),
-    ])
-
-    tensor = transform(img)
+    
+    crop_size = new_dino_size(img.size)
+    img_cropped = transforms.CenterCrop(crop_size)(img)
+    tensor = transform(img_cropped)
 
     torch.save(tensor, dst_path)
+
+if __name__ == "__main__":
+    image_files = [f for f in os.listdir(SRC_DIR) if f.lower().endswith((".jpg", ".jpeg", ".png"))]
+    
+    with ProcessPoolExecutor() as executor:
+        conv = {executor.submit(convert, fname): fname
+                for fname in image_files[:1000]}
+        for unit in tqdm(as_completed(conv), desc="Transform to tensors"):
+            unit.result()
+    
